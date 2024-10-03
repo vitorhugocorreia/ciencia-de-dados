3dfe4c6e88ad662ee28acfcd43d15b2195e8aebf
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2024-10-03 10:27:56 -0300

    salvar

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,7 @@
+const url ='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+
+async function visualizarInformacoesGblobais() {
+    
+}
+
+visualizarInformacoesGblobais();
