cb7f58ce1ef637bb537cc8c7093b7cf1919636d9
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2024-10-17 10:38:42 -0300

    spam

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -5,7 +5,7 @@ async function visualizarInformacoesGblobais() {
     const dados = await res.json()
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container_texto')
-    paragrafo.innerHTML = 'Você sabia que o muno tem ${dados.total_pesoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas em alguma rede social e passam em média ${dados.tempo_medio} hora conectadas.'
+    paragrafo.innerHTML = 'Você sabia que o muno tem <span>${dados.total_pesoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> em alguma rede social e passam em média <span>${dados.tempo_medio}</span> hora conectadas.'
 
     const container = document.getElementById('grafico-container')
     container.appenChil(paragrafo)
