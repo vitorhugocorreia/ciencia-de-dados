a87fe0cde6fbd899c3b84ad8b3a005bac3122e31
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2024-10-17 10:29:38 -0300

    res

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,8 +1,8 @@
 const url ='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function visualizarInformacoesGblobais() {
-    const re = await fetch(url)
-    const dados = await re.json()
+    const res = await fetch(url)
+    const dados = await res.json()
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container_texto')
     paragrafo.innerHTML = 'Você sabia que o muno tem ${dados.total_pesoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas em alguma rede social e passam em média ${dados.tempo_medio} hora conectadas.'
