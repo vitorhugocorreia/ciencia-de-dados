ce6fa2d63887fd604ece4be32543ffb68fefbd47
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2024-10-24 10:39:45 -0300

    oitdo

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,6 +1,6 @@
 const url ='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function visualizarInformacoesGblobais() {
+async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
 
@@ -12,8 +12,8 @@ async function visualizarInformacoesGblobais() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container_texto')
-    paragrafo.innerHTML = 'Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões </span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões </span> em alguma rede social e passam em média <span>${horas}</span> horas e </span>${minutos}</span> minutos conectadas.
-    <br> Isso significa que aproximadamente ${porcentagemConectada}% de pessoas estão conectadas em alguma rede social.
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões </span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões </span> em alguma rede social e passam em média <span>${horas}</span> horas e <span>${minutos}</span> minutos conectadas.
+    <br> Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
 
     const container = document.getElementById('grafico-container')
     container.appenChil(paragrafo)
