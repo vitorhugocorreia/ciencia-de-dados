90cbaf963021032948a9aaa5871f3ded920a8578
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2024-10-24 10:34:51 -0300

    oknwhheh

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,13 +3,21 @@ const url ='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-gl
 async function visualizarInformacoesGblobais() {
     const res = await fetch(url)
     const dados = await res.json()
+
+    const pessoasConectadas = (dados.total_pessoas_conectadas ) / 1e9
+    const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
+    const horas = parseInt (dados.tempo_medio)
+    const minutos = Math.round ((dados.tempo_medio - horas) *100)
+    const porcentagemConectada= ((pessoasConectadas / pessoasNoMundo) *100).toFixed(2)
+
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container_texto')
-    paragrafo.innerHTML = 'Você sabia que o muno tem <span>${dados.total_pesoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.'
+    paragrafo.innerHTML = 'Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões </span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões </span> em alguma rede social e passam em média <span>${horas}</span> horas e </span>${minutos}</span> minutos conectadas.
+    <br> Isso significa que aproximadamente ${porcentagemConectada}% de pessoas estão conectadas em alguma rede social.
 
     const container = document.getElementById('grafico-container')
     container.appenChil(paragrafo)
-    //tem um erro ai em cima
+    
 }
 
-visualizarInformacoesGblobais();
+visualizarInformacoesGlobais();
