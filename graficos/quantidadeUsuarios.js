ceb275be0cda6663037cf00e71e49da46a8d88cd
vitorhugocorreia <105368311+vitorhugocorreia@users.noreply.github.com>
2024-11-21 10:18:50 -0300

    a

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -11,7 +11,7 @@ async function quantidadeUsuariosPorRede() {
         {
             x: nomeDasRedes,
             y: quantidadeDeUsuarios,
-            type: 'bar',
+            type: "bar",
             marker: {
                 color: getCSS('--primary-color')
             }
